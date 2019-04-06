b851ee3e52c5c35263316d80732b0f26a3d9371b
shuichen17 <shuichen17@gmail.com>
2019-04-05 22:16:30 -0700

    finish the file

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,46 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-};
+   var str = '';
+  if(typeof obj === 'undefined' || typeof obj === 'function'){
+  	return undefined;
+  } 
+  else if (typeof obj === 'string'){
+  	return '\"' + obj + '\"';
+  }
+  else if (typeof obj !== 'object'){
+  	return '' + obj;
+  } 
+  else if (obj === null){
+  	return 'null';
+  }
+  else if(Array.isArray(obj) === true){
+      
+  		for(var i = 0; i < obj.length; i++){
+  			var temp = stringifyJSON(obj[i]);
+  			str = str + temp;
+  			if(i !== obj.length - 1){
+  				str = str + ',';
+  			}
+  		}
+  		
+  		return '[' + str + ']';
+  	} else {
+  		var flag = false;
+  		for(var key in obj){
+  			var temp = stringifyJSON(obj[key]);
+  			if(typeof temp !== 'undefined'){
+  				flag = true;
+  				str = str + '\"' + key + '\":' + temp + ','; 
+  			}
+  		}
+  		//if obj not empty, the last ',' need to delete
+  		if(flag)
+  			str = str.slice(0, -1);
+  		}
+  		return '{' + str + '}';
+  	};
+  
+
+
+
